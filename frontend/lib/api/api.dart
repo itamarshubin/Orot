399ad0aac4a3e5c73f564c1be75e183b65e22dc2
Yechiel Babani <yechielb2000@gmail.com>
2024-12-22 17:56:08 +0200

    remove print

--- frontend/lib/api/api.dart
+++ frontend/lib/api/api.dart
@@ -1,36 +1,37 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:orot/types/profiles.dart';
 
 const String baseUrl = "http://10.0.2.2:5000";
 
 Future<Profile> fetchProfile(String name) async {
-    final res = await http.get(Uri.parse("$baseUrl/profiles/$name"));
+  final res = await http.get(Uri.parse("$baseUrl/profiles/$name"));
 
-    if (res.statusCode == 200) {
-      return Profile.fromJson(jsonDecode(res.body));
-    } else {
-      throw Exception('Failed to fetch joke');
-    }
+  if (res.statusCode == 200) {
+    return Profile.fromJson(jsonDecode(res.body));
+  } else {
+    throw Exception('Failed to fetch joke');
+  }
 }
 
 Future<FamilyProfile> fetchFamilyProfile(String name) async {
-    final res = await http.get(Uri.parse("$baseUrl/profiles/$name/families"));
+  final res = await http.get(Uri.parse("$baseUrl/profiles/$name/families"));
 
-    if (res.statusCode == 200) {
-      return FamilyProfile.fromJson(jsonDecode(res.body));
-    } else {
-      throw Exception('Failed to fetch joke');
-    }
+  if (res.statusCode == 200) {
+    return FamilyProfile.fromJson(jsonDecode(res.body));
+  } else {
+    throw Exception('Failed to fetch joke');
+  }
 }
 
 Future fetchMeetingDates(String name) async {
-    final res = await http.get(Uri.parse("$baseUrl/meetings/$name"));
+  final res = await http.get(Uri.parse("$baseUrl/meetings/$name"));
 
-    if (res.statusCode == 200) {
-      print(jsonDecode(res.body));
-      return jsonDecode(res.body);
-    } else {
-      throw Exception('Failed to fetch joke');
-    }
+  if (res.statusCode == 200) {
+    // print(jsonDecode(res.body));
+    return jsonDecode(res.body);
+  } else {
+    throw Exception('Failed to fetch joke');
+  }
 }
