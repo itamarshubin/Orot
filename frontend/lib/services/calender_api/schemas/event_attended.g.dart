ce42ecf5126a09903e2180ed36ae958314a60643
Yechiel Babani <yechielb2000@gmail.com>
2024-12-22 17:03:30 +0200

    upload g.darts to git

---
+++ frontend/lib/services/calender_api/schemas/event_attended.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'event_attended.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+EventAttended _$EventAttendedFromJson(Map<String, dynamic> json) =>
+    EventAttended(
+      displayName: json['displayName'] as String,
+      email: json['email'] as String,
+    );
+
+Map<String, dynamic> _$EventAttendedToJson(EventAttended instance) =>
+    <String, dynamic>{
+      'displayName': instance.displayName,
+      'email': instance.email,
+    };
