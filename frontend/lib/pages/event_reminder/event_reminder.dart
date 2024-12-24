431bbc493f5ec31350c559bbee8e4f965ca93f46
Yechiel Babani <yechielb2000@gmail.com>
2024-12-24 22:40:34 +0200

    add flutter_local_notifications  firebase_messaging packages

---
+++ frontend/lib/pages/event_reminder/event_reminder.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class EventReminder extends StatefulWidget {
+  const EventReminder({super.key});
+
+  @override
+  State<EventReminder> createState() => _EventReminderState();
+}
+
+class _EventReminderState extends State<EventReminder> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
