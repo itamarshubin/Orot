edb369369f83866966625c632d41b775cdb2bf5c
Yechiel Babani <yechielb2000@gmail.com>
2024-12-25 23:20:38 +0200

    fix import

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:orot/firebase_options.dart';
+import 'package:orot/pages/home/home_page.dart';
 import 'package:orot/pages/login/login_page.dart';
 import 'package:orot/user_provider.dart';
 import 'package:provider/provider.dart';
