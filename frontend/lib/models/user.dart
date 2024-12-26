cacd9de2f34f8c19d425956c98b6b82439f4a2ec
Yechiel Babani <yechielb2000@gmail.com>
2024-12-26 12:49:32 +0200

    add json serialize for user model

--- frontend/lib/models/user.dart
+++ frontend/lib/models/user.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 import 'district.dart';
 
+part 'user.g.dart';
+
 enum UserPermission { admin, coordinator, volunteer }
 
+@JsonSerializable()
 class User {
   final String uid;
   final UserPermission permission;
@@ -19,16 +23,7 @@ class User {
     this.district,
   });
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    final FirebaseAuth auth = FirebaseAuth.instance;
-    return User(
-      uid: json['uid'],
-      permission: UserPermission.values.byName(json['permission']),
-      name: json['name'] ?? auth.currentUser?.displayName,
-      email: json['email'],
-      district: json['district'] != null
-          ? District(id: json['district']['id'], name: json['district']['name'])
-          : null,
-    );
-  }
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
 }
