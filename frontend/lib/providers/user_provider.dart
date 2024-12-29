ea432f75bd5ad891b48cf39bd1cd901700194670
Itamar Shubin <itamarshubin@gmail.com>
2024-12-29 03:15:24 +0200

    check for instance before trying to get userData

--- frontend/lib/providers/user_provider.dart
+++ frontend/lib/providers/user_provider.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flutter/material.dart';
 import 'package:orot/models/user.dart';
 import 'package:orot/services/auth_service.dart';
@@ -10,8 +11,10 @@ class UserProvider with ChangeNotifier {
   String? get userName => _user?.name;
 
   UserPermission? get userPermission => _user?.permission;
+  final _auth = firebase_auth.FirebaseAuth.instance;
 
   Future<User?> getUserData() async {
+    if (_auth.currentUser == null) return _user;
     final response = await AuthService().getCurrentUser();
     if (response != null) {
       _user = User.fromJson(response);
