34f43cd14b46b4d7b6fc39b88aa45bf584f6746e
Yechiel Babani <yechielb2000@gmail.com>
2025-01-03 16:01:26 +0200

    update main page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -15,14 +18,14 @@ Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   //TODO: Remove this lines when deploying to production
-  // try {
-  //   FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
-  //   FirebaseFunctions.instance.useFunctionsEmulator('localhost', 5001);
-  //   await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-  // } catch (e) {
-  //   // ignore: avoid_print
-  //   print(e);
-  // }
+  try {
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+    FirebaseFunctions.instance.useFunctionsEmulator('localhost', 5001);
+    await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  } catch (e) {
+    // ignore: avoid_print
+    print(e);
+  }
 
   FlutterNativeSplash.remove();
 
