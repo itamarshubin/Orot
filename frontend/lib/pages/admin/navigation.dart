4b2a6a0aaae9742af0883b5c5ee89b42e7c13dc2
Yechiel Babani <yechielb2000@gmail.com>
2025-01-03 15:29:13 +0200

    reduce code in admin navigation page

--- frontend/lib/pages/admin/navigation.dart
+++ frontend/lib/pages/admin/navigation.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:orot/pages/admin/admin_page.dart';
 import 'package:orot/pages/admin/district_list.dart';
-import 'package:orot/pages/coordinator/volunteers_list/volunteers_list.dart';
 import 'package:orot/pages/profile/profile_page.dart';
-import 'package:orot/pages/volunteer/home/home_page.dart';
-import 'package:orot/pages/volunteer/visits_history/visits_history_page.dart';
 
 class AdminNavigation extends StatefulWidget {
   const AdminNavigation({super.key});
@@ -14,59 +11,32 @@ class AdminNavigation extends StatefulWidget {
 }
 
 class _AdminNavigationState extends State<AdminNavigation> {
-  Widget currentChild = DistrictList();
+  late Widget currentChild;
   int currentIndex = 2;
+  List<Widget> pages = [
+    const ProfilePage(),
+    const AdminPage(),
+    const DistrictList(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: currentChild,
+        body: pages[currentIndex],
         bottomNavigationBar: BottomNavigationBar(
-          onTap: (index) => _onItemTapped(context, index),
+          onTap: (index) => setState(() => currentIndex = index),
           iconSize: 25,
           currentIndex: currentIndex,
           selectedItemColor: Color(0xFF205273),
           unselectedItemColor: Colors.grey,
           items: const [
             BottomNavigationBarItem(
-              tooltip: 'פרופיל',
-              icon: Icon(Icons.person),
-              label: '',
-            ),
+                tooltip: 'פרופיל', icon: Icon(Icons.person), label: ''),
             BottomNavigationBarItem(
-              tooltip: 'עריכה',
-              icon: Icon(Icons.edit),
-              label: '',
-            ),
+                tooltip: 'עריכה', icon: Icon(Icons.edit), label: ''),
             BottomNavigationBarItem(
-              tooltip: 'מסך ראשי',
-              icon: Icon(Icons.home),
-              label: '',
-            ),
+                tooltip: 'מסך ראשי', icon: Icon(Icons.home), label: ''),
           ],
         ));
-  }
-
-  void _onItemTapped(BuildContext context, int index) {
-    switch (index) {
-      case 0:
-        setState(() {
-          currentIndex = 0;
-          currentChild = const ProfilePage();
-        });
-        break;
-      case 1:
-        setState(() {
-          currentIndex = 1;
-          currentChild = const AdminPage();
-        });
-        break;
-      case 2:
-        setState(() {
-          currentIndex = 2;
-          currentChild = DistrictList();
-        });
-        break;
-    }
   }
 }
