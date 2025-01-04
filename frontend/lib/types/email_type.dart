336d1734b8df50b15dc21bb7ff56fdf19643d265
Yechiel Babani <yechielb2000@gmail.com>
2025-01-04 22:34:06 +0200

    create email type

---
+++ frontend/lib/types/email_type.dart
@@ -0,0 +1,8 @@
+extension Email on String {
+  static String emailRegex =
+      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$";
+
+  bool isValid() {
+    return RegExp(emailRegex).hasMatch(this);
+  }
+}
