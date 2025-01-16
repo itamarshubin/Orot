9d46d2af2f880fd84c978df9df972e8aa41f19a9
Yechiel Babani <yechielb2000@gmail.com>
2025-01-16 22:53:31 +0200

    remove dup buttons

--- frontend/lib/pages/admin/admin_page.dart
+++ frontend/lib/pages/admin/admin_page.dart
@@ -1,12 +1,9 @@
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
 import 'package:orot/models/user.dart';
 import 'package:orot/pages/admin/add_coordinator_page.dart';
 import 'package:orot/pages/admin/add_district_page.dart';
 import 'package:orot/pages/admin/add_family_page.dart';
 import 'package:orot/pages/admin/add_volunteer_page.dart';
-import 'package:orot/pages/volunteer/navigation.dart';
 import 'package:orot/providers/user_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -24,12 +21,12 @@ class AdminPage extends StatelessWidget {
             spacing: 20,
             children: [
               if (userProvider.userPermission == UserPermission.admin)
-                _addFamilyButton(context),
+                _addEntityButton(context, 'הוספת משפחה', AddFamilyPage()),
               if (userProvider.userPermission == UserPermission.admin)
-                _addCoordinatorButton(context),
+                _addEntityButton(context, 'הוספת רכזת', AddCoordinatorPage()),
               if (userProvider.userPermission == UserPermission.admin)
-                _addDistrictButton(context),
-              _addUserButton(context),
+                _addEntityButton(context, 'הוספת מחוז', AddDistrictPage()),
+              _addEntityButton(context, 'הוספת משתמש', AddVolunteerPage()),
             ],
           ),
         ),
@@ -37,60 +34,15 @@ class AdminPage extends StatelessWidget {
     });
   }
 
-  Widget _addUserButton(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) => AddVolunteerPage()));
-          },
-          child: const Text('הוספת משתמש')),
-    );
-  }
-
-  Widget _addCoordinatorButton(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) =>
-                        const AddCoordinatorPage()));
-          },
-          child: const Text('הוספת רכזת')),
-    );
-  }
-
-  Widget _addDistrictButton(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) => AddDistrictPage()));
-          },
-          child: const Text('הוספת מחוז')),
-    );
-  }
-
-  Widget _addFamilyButton(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) => AddFamilyPage()));
-          },
-          child: const Text('הוספת משפחה')),
+  Widget _addEntityButton(BuildContext context, String text, Widget page) {
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        child: ElevatedButton(
+            onPressed: () => Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (_) => page)),
+            child: Text(text)),
+      ),
     );
   }
 }
