4cf076f4a5c78e0a3ed297404181fe4419aef2f6
Yechiel Babani <yechielb2000@gmail.com>
2025-02-22 18:48:11 +0200

    add main_button

---
+++ frontend/lib/components/main_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
+
+enum MainButtonSize { big, small }
+
+class MainButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPress;
+  final MainButtonSize size;
+  final bool disabled;
+
+  const MainButton({
+    super.key,
+    required this.text,
+    required this.onPress,
+    this.size = MainButtonSize.big,
+    this.disabled = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.sizeOf(context).height;
+    return InkWell(
+      onTap: disabled ? null : onPress,
+      child: Container(
+          alignment: Alignment.center,
+          width: MediaQuery.sizeOf(context).width * 0.75,
+          height: screenHeight * (size == MainButtonSize.big ? 0.06 : 0.05),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: disabled
+                  ? [Color(0xFF44474C), Color(0xFF777B7C)]
+                  : [Color(0xFF205273), Color(0xFF3C9BD9)],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+            ),
+            borderRadius: BorderRadius.circular(30.sh),
+            boxShadow: disabled
+                ? null
+                : [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0, 2),
+                      blurRadius: 6,
+                    ),
+                  ],
+          ),
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Text(
+              text,
+              style: GoogleFonts.openSans(
+                fontWeight: FontWeight.w500,
+                fontSize: 25,
+                color: disabled ? Colors.white70 : Colors.white,
+              ),
+            ),
+          )),
+    );
+  }
+}
